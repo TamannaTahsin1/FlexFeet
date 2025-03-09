18b8f7da6c2f7dc4d60a6bcbc7b855bf1767bfc7
Tamanna Tahsin <138390753+TamannaTahsin1@users.noreply.github.com>
2025-03-09 19:45:15 +0600

    collection section initialized

---
+++ src/Components/SectionComponents/Collections/Collections.jsx
@@ -0,0 +1,28 @@
+import img1 from '../../../assets/collections/28_DZGrowU.jpg';
+import img2 from '../../../assets/collections/acne_studios.jpg';
+import img3 from '../../../assets/collections/aime_leon_dore (1).jpg';
+import img4 from '../../../assets/collections/aime_leon_dore.jpg';
+import img5 from '../../../assets/collections/air_jordans.jpg';
+import img6 from '../../../assets/collections/alpgaomegacirlce.png';
+import img7 from '../../../assets/collections/bagpacks.jpg';
+import img8 from '../../../assets/collections/bags.jpg';
+import img9 from '../../../assets/collections/bape.jpg';
+import img10 from '../../../assets/collections/beanies.jpg';
+import img11 from '../../../assets/collections/belts.jpg';
+import img12 from '../../../assets/collections/billionaire_boys.jpg';
+import img13 from '../../../assets/collections/drip-project.jpg';
+import img14 from '../../../assets/collections/for_her_icon.jpg';
+import img15 from '../../../assets/collections/henney_bear_icon.jpg';
+import img16 from '../../../assets/collections/new_circle_icons.png';
+import img17 from '../../../assets/collections/thekhwaab.jpg';
+import img18 from '../../../assets/collections/zouk_icon.jpg';
+
+const Collections = () => {
+    return (
+        <div className='p-5'>
+            
+        </div>
+    );
+};
+
+export default Collections;
